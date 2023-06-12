8138dc1960ab24ed22c4330e934764ea349cae9f
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-13 00:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  for(let x of sampleObject){
+  for(let x in sampleObject){
 	  if(x=== key)
 		  return true;
   } 
